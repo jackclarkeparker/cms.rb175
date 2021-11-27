b9c61a536951b2b12b533eae0da32f49e8e03492
Jack <jclarkeparker@gmail.com>
2021-11-28 12:38:05 +1300

    LS solution stored files in a directory called 'data' which conforms to good project organisation tropes. They also accessed their files in a very diff way from me. To begin with they set out an absolute path for the project directory using 'File.expand_path('..', __FILE__)' which is an idiom that is good to remember, but a bit complex and not important to grok. They stored this in a local variable initialised in the top level scope, which is then accessible in their route. I wonder why they do it there and not inside the route. If they want it accessible elsewhere, wouldn't it make sense to make the variable a constant? Unless the variable can change inside the routes...? They then combined the root variable with the component of the file paths that would lead to the data directory from the project directoy, and passed this as an arg to glob which returns an array of all matching files

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'tilt/erubis'
+
+root = File.expand_path("..", __FILE__)
 
 get "/" do
-  @files = Dir.children("documents").sort
-  erb :home
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
