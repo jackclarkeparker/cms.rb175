738587de3989949d5dc3c3d4b3030b273e2d6795
Jack <jclarkeparker@gmail.com>
2021-11-28 12:54:26 +1300

    Made data the public directory so that it's contents are accesible without specified sinatra routes. Updated the index view to turn each file into a link, whose reference takes the user to the static file as supported by the public directory setting. Also put a bit of content into each file.

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'tilt/erubis'
 
 root = File.expand_path("..", __FILE__)
 
+configure do
+  set :public_folder, root + '/data'
+end
+
 get "/" do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
