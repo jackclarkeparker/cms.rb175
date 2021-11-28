ca797f86525506d8bea9f32c658de6c91a6f0bad
Jack <jclarkeparker@gmail.com>
2021-11-28 13:31:25 +1300

    LS solution involved defining a route for the files, not serving them through the public folder. They read them using their absolute paths, I guess conforming to some pattern there. Their reference for the anchor links began with a / and so was absolute, mine was relative - another thing to practice. Finally, I didn't set a content type! This affects how the page renders, I didn't realise, I thought I was already rendering as plain text! In order to make this change, you've got to use 'headers' which references a hash of all the HTTP headers that will be submitted in your response (I suppose.?)

--- app.rb
+++ app.rb
@@ -4,13 +4,15 @@ require 'tilt/erubis'
 
 root = File.expand_path("..", __FILE__)
 
-configure do
-  set :public_folder, root + '/data'
-end
-
 get "/" do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
   erb :index
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
