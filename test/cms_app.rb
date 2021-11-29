5234d0fea781b3cff27f03dc6fe56bc512ec444e
Jack <jclarkeparker@gmail.com>
2021-11-30 09:59:41 +1300

    My attempt at adding first tests to cms project

---
+++ test/cms_app.rb
@@ -0,0 +1,66 @@
+ENV["RACK_ENV"] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../cms'
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def setup
+    @root = File.expand_path('../..', __FILE__)
+    @file_paths = Dir.glob(@root + "/data/*")
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    # Headers
+      # Content-Type is what they tested for before. Anything else?
+      # When I run `headers` in sinatra, it shows that only one response
+      # header is already accessible, but presumably others are also used.
+      
+      # Yeah. Maybe a location would be good to check with a redirect?
+
+      # I've just looked at the response headers included when we load the
+      # index. There aren't many. There is Content-Length?
+      # There's also a few X-**** response headers that I've never heard of.
+      # For now, I suppose I'll stick to
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    # Body
+      # Do we write out a bunch of HTML?
+      # Do we hard code the files that we've been told we'll work with?
+      # Do we test component of the body, rather than the body in full? <--- THIS
+
+      # Plan is this -> Put this aside for now, these questions are alive.
+      # Instead, go take a look at the other test, and then once you've tinkered
+      # there as well, compare notes with the implementation suggestions.
+    
+    file_names = @file_paths.map { |path| File.basename(path) }
+    assert file_names.all? { |file| last_response.body.include? file }
+  end
+
+  def test_content # || test_files || test_file
+    # Question of, do we hard code each file explicitly in this test method?
+    # Do we carry out preparation first? We kind of need to if we're issuing
+    # a request to a random route. If we are hardcoding the routes, then we
+    # won't need to do any prep.
+    # Maybe we do it hardcoded now?
+    # Maybe we test the idea of doing prep first, and see if that works?
+    # Then hardcode?
+    @file_paths.each do |path|
+      route = File.basename(path)
+
+      get "/#{route}"
+      assert_equal 200, last_response.status
+      assert_equal "text/plain", last_response["Content-Type"]
+      # assert(last_response.body.include? File.read(path))
+      assert_includes last_response.body, File.read(path)
+    end
+  end
+end
