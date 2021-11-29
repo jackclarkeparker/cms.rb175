e27e18d19d3f2e52c18e791f01967091663b3d68
Jack <jclarkeparker@gmail.com>
2021-11-30 10:28:18 +1300

    rearranged gems in gemfile, grouped categorically. Renamed AppTest to CMSTest, a hangover from before I changed the name of the application file. BIG THINGS. LS hardcoded their tests, they used literals. This leads to tests that are a lot harder to misinterpret; if this happens as a result of a misunderstanding of how a particular method is working say, then your tests will tell you that your code is working even when it's not, can lead to annoying bugs, best to use simple literals in tests.

--- test/cms_app.rb
+++ test/cms_app.rb
@@ -5,12 +5,16 @@ require 'rack/test'
 
 require_relative '../cms'
 
-class AppTest < Minitest::Test
+class CMSTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
+
+=begin
+
+MY ORIGINAL TESTS
 
   def setup
     @root = File.expand_path('../..', __FILE__)
@@ -32,20 +36,27 @@ class AppTest < Minitest::Test
       # There's also a few X-**** response headers that I've never heard of.
       # For now, I suppose I'll stick to
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+  
     # Body
-      # Do we write out a bunch of HTML?
-      # Do we hard code the files that we've been told we'll work with?
-      # Do we test component of the body, rather than the body in full? <--- THIS
 
-      # Plan is this -> Put this aside for now, these questions are alive.
-      # Instead, go take a look at the other test, and then once you've tinkered
-      # there as well, compare notes with the implementation suggestions.
+    # Do we hard code the files that we've been told we'll work with?
+    # Do we test component of the body, rather than the body in full?
+    
+      #   Actually a combination of these two ^^
+      #     We want to hardcode literals into our tests so that we don't leave
+      #     room for a misinterpretation of how code works to filter into our
+      #     tests, such that they suggest they're working, when in reality they
+      #     are not. VERY difficult to squash that bug.
+
+    # Plan is this -> Put this aside for now, these questions are alive.
+    # Instead, go take a look at the other test, and then once you've tinkered
+    # there as well, compare notes with the implementation suggestions.
     
     file_names = @file_paths.map { |path| File.basename(path) }
     assert file_names.all? { |file| last_response.body.include? file }
   end
 
-  def test_content # || test_files || test_file
+  def test_viewing_text_document # || test_files || test_file
     # Question of, do we hard code each file explicitly in this test method?
     # Do we carry out preparation first? We kind of need to if we're issuing
     # a request to a random route. If we are hardcoding the routes, then we
@@ -62,5 +73,27 @@ class AppTest < Minitest::Test
       # assert(last_response.body.include? File.read(path))
       assert_includes last_response.body, File.read(path)
     end
+  end
+
+=end
+
+  def test_index
+    get "/"
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+
+  def test_viewing_text_document
+    get "/history.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "This has all happened before..."
   end
 end
