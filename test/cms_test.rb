f09be76c0befee0f2030cd6291edbedff72fce1c
Jack <jclarkeparker@gmail.com>
2021-12-02 10:11:12 +1300

    Slight refactoring and rewrites of tests, they were basically similar. LS tested for body content on the edit view, ensuring some desired HTML was present. In some instances I would assert for status when they didn't, sometimes vice versa. They also separated their edit view text and their updating doc test, I had these in one big test, good to separate them out.

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -7,17 +7,92 @@ require 'fileutils'
 
 require_relative '../cms'
 
-def create_document(name, content = "")
-  File.open(File.join(data_path, name), "w") do |file|
-    file.write(content)
-  end
-end
-
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
+  end
+
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
+  def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
+    get "/"
+    
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "edit"
+  end
+
+  def test_viewing_text_document
+    create_document "history.txt", "This has all happened before..."
+
+    get "/history.txt"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "This has all happened before..."
+  end
+
+  def test_viewing_nonexistent_document
+    get "/madeupfile.ext"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "madeupfile.ext does not exist."
+
+    get "/"
+    refute_includes last_response.body, "madeupfile.ext does not exist."    
+  end
+
+  def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+
+  def test_editing_document
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/changes.txt", { new_content: "testing" }
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "changes.txt has been updated!"
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "testing"
   end
 
 =begin
@@ -84,81 +159,4 @@ MY ORIGINAL TESTS
   end
 
 =end
-
-  def setup
-    FileUtils.mkdir_p(data_path)
-  end
-
-  def teardown
-    FileUtils.rm_rf(data_path)
-  end
-
-  def test_index
-    create_document "about.md"
-    create_document "changes.txt"
-
-    get "/"
-    
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.md"
-    assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "edit"
-  end
-
-  def test_viewing_text_document
-    create_document "history.txt", "This has all happened before..."
-
-    get "/history.txt"
-
-    assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "This has all happened before..."
-  end
-
-  def test_viewing_nonexistent_document
-    get "/madeupfile.ext"
-    assert_equal 302, last_response.status
-
-    get last_response["Location"]
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "madeupfile.ext does not exist."
-
-    get "/"
-    refute_includes last_response.body, "madeupfile.ext does not exist."    
-  end
-
-  def test_viewing_markdown_document
-    create_document "about.md", "# Ruby is..."
-
-    get "/about.md"
-
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "<h1>Ruby is...</h1>"
-  end
-
-  def test_editing_document
-    # root = File.expand_path("../..", __FILE__)
-    # file_path = root + "/data/changes.txt"
-    # original_content = File.read(file_path)
-
-    # THIS ^^^ was to collect the original content of the file so that we could
-    # later restore it.
-
-    create_document "changes.txt"
-
-    get "/changes.txt/edit"
-    assert_equal 200, last_response.status
-    
-    post "/changes.txt", { new_content: "testing" }
-    assert_equal 302, last_response.status
-
-    get last_response["Location"]
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "changes.txt has been updated!"
-
-    get "/changes.txt"
-    assert_includes last_response.body, "testing"
-  end
 end
